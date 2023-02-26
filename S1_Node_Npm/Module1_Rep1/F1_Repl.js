eba454d300294a83bd8a24eb77f220ae8333aae0
Amul Op <amulsharmarr67@gmail.com>
2023-02-26 11:27:05 +0530

    Repl in NodeJS

---
+++ S1_Node_Npm/Module1_Rep1/F1_Repl.js
@@ -0,0 +1,6 @@
+//REPL - NODEJS
+// Repl stands for READ, EVALUATE, PRINT, LOOP
+//Type node to enter in the repl
+// you can create variables, write javascript expressions
+// Press tab two times to see all the global modules provided by the NodeJS.
+// .exit for exiting the repl
